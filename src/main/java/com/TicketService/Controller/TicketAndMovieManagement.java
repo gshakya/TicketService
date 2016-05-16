68aa0ef96eb67187d1dec25ef6ffab825ad72648
Gunjan Shakya <gr.skya@gmail.com>
2016-05-16 12:27:37 -0500

    Buy Ticket Feature Added .. Need some modification with Anonymous User

--- src/main/java/com/TicketService/Controller/TicketAndMovieManagement.java
+++ src/main/java/com/TicketService/Controller/TicketAndMovieManagement.java
@@ -1,5 +1,8 @@
 package com.TicketService.Controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,15 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.TicketService.Model.Customer;
 import com.TicketService.Model.Movie;
-import com.TicketService.Service.TicketFinder;
+import com.TicketService.Model.Ticket;
+import com.TicketService.ServiceImpl.TicketFinderServiceImpl;
 
 @Controller
 // @RequestMapping("/movie")
 public class TicketAndMovieManagement {
 
 	@Autowired
-	private TicketFinder ticketFinder;
+	private TicketFinderServiceImpl ticketFinder;
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
@@ -34,15 +39,32 @@ public class TicketAndMovieManagement {
 	}
 
 	@RequestMapping(value = "/movie/findMovie", method = RequestMethod.GET)
-	public String findMovie(@RequestParam("movieName") String movieName, Model model) {
+	public String findMovie(@RequestParam("movieName") String movieName, @RequestParam("searchDate") String searchDate,
+			Model model) throws ParseException {
 		logger.debug("I'm here");
-		System.out.println("I'm Here");
-		List<Movie> movies = ticketFinder.findByName(movieName);
-		for (Movie m : movies){
-			System.out.println("Movie:"+m);
+		List<Movie> movies;
+		if (searchDate == "") {
+			movies = ticketFinder.findByName(movieName);
+		} else {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Date sDate = simpleDateFormat.parse(searchDate);
+			movies = ticketFinder.findByNameAndDate(movieName, sDate);
 		}
-		
+		System.out.println(movies.size());
+
+		model.addAttribute("movies", movies);
+
 		return "movieList";
+	}
+	
+	@RequestMapping (value = "/movie/buyTicket" , method = RequestMethod.POST , headers= { "Accept=application/json" })
+	public String buyTicket(@RequestParam("movieId")  String movieId, Model model ){
+		System.out.println("Buy Ticket Conformation" + movieId);
+		Customer c = new Customer();
+		Ticket t = ticketFinder.buyTicket(Long.parseLong(movieId), c);	
+		model.addAttribute("ticket", t);
+		return "ticketConformation";
+		
 	}
 
 }
--- src/main/webapp/WEB-INF/views/movieList.jsp
+++ src/main/webapp/WEB-INF/views/movieList.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,12 +8,58 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<h1>Find Movie</h1>
+	<form action="${pageContext.request.contextPath}/movie/findMovie"
+		method="get" name="searchMovie">
 
-	<form action="${pageContext.request.contextPath}/movie/findMovie" method="get">
-		<h1>Find Movie</h1>
-		<br> Movie Name <input type="text" name="movieName"> <input
-			type="submit" value="Find">
+
+		<br> Movie Name <input type="text" name="movieName"> <br>Date
+		<input type="datetime-local" path="tempdate"
+			class="form-control margin-bottom-20" name="searchDate"
+			placeholder="Date" /> <input type="submit" value="Find"> <br>
 	</form>
+	<table padding="6px" bordercolor="black">
+		<tr>
+			<td>Movie Name</td>
+			<td>Movie Description</td>
+			<td>Hall</td>
+			<td>Screening Date</td>
+			<td>Total Seat</td>
+			<td>Price</td>
+			<td></td>
+		</tr>
 
+		<c:forEach items="${movies}" var="movie">
+
+			<tr>
+				<td>${movie.movieName}</td>
+				<td>${movie.description}</td>
+				<td>${movie.details.hall}</td>
+				<td>${movie.details.screeningDate}</td>
+				<td>${movie.details.totalSeats}</td>
+				<td>${movie.details.price}</td>
+				<td>
+					<!-- <form method="post" name="buyTicket"action "${pageContext.request.contextPath}/movie/buyTicket">
+ --> <input onclick="buyTicket(${movie.movieId})" type="submit">
+					<!-- </form> -->
+
+				</td>
+			</tr>
+		</c:forEach>
+	</table>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script> 
+	<script type="text/javascript">
+function  buyTicket(id){
+	$.ajax({type : "POST",
+		contentType:"application/json; charset=utf-8",
+		url : "${pageContext.request.contextPath}/movie/buyTicket?movieId="+id,
+		success : function(response) {
+			console.log("MovieId Sent "+id);
+		}
+			}); 
+ }
+
+
+</script>
 </body>
 </html>
